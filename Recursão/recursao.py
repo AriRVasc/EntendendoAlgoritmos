f6a119d3b3fb84fa14f40219fc74fe07b2ba4132
Ariadne Rodrigues Vasconcelos <ariadnerv17@gmail.com>
2023-10-29 22:35:54 -0300

    Capítulo 3 - Recursão

--- Recursão/recursao.py
+++ Recursão/recursao.py
@@ -1,14 +1,23 @@
 #Backup do sistema de vendas
-
-meses_sistema = ["jan2022", "fev2022", "mar2022", "abr2022", "mai2022", "jun2022", "jul2022", "ago2022", "set2022", "out2022", "nov2022", "dez2022"]
+meses_sistema = ["jan2023", "fev2023", "mar2023", "abr2023", "mai2022", "jun2022", "jul2022", "dez2022"]
 print(len(meses_sistema))
 
 import os #percorrer pastas no computador
 
-arquivos_extraidos = []
+lista_pastas = []
+
 def pegar_arquivos_pasta(pasta):
-    arquivo = os.listdir(pasta)
-    print(arquivo)
-    pass
+    arquivos = os.listdir(pasta)
+    for arquivo in arquivos:
+        if "txt" in arquivo and "Vendas" in arquivo:
+            nome_mes = arquivo.split()[-1].replace(".txt", "")
+            lista_pastas.append(nome_mes)
+        elif "txt" not in arquivo:
+            pegar_arquivos_pasta(f'{pasta}/{arquivo}')
 
 pegar_arquivos_pasta("Arquivos")
+print(lista_pastas)
+
+for mes in meses_sistema:
+    if mes not in lista_pastas:
+        print(mes)
