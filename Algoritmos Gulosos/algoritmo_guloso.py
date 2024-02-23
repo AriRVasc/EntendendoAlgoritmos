dffc1952c95b35b4c0abbd9d31d91ef1029b43ef
ari <ariadnerv17@gmail.com>
2024-02-23 00:49:16 -0300

    Ajuste no algoritmo

--- Algoritmos Gulosos/algoritmo_guloso.py
+++ Algoritmos Gulosos/algoritmo_guloso.py
@@ -1,1 +1,25 @@
-ssss
+estados_abranger = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+estacoes = {}
+estacoes["kum"] = set(["id", "nv", "ut"])
+estacoes["kdois"] = set(["wa", "id", "mt"])
+estacoes["ktres"] = set(["or", "nv", "ca"])
+estacoes["kquatro"] = set(["nv", "ut"])
+estacoes["kcinco"] = set(["ca", "az"])
+
+estacoes_final = set()
+
+while estados_abranger:
+    melhor_estacao = None
+    estados_cobertos = set()
+
+    for estacao, estados_por_estacao in estacoes.items():
+        coberto = estados_abranger & estados_por_estacao
+        print(coberto)
+        if len(coberto) > len(estados_cobertos):
+            melhor_estacao = estacao
+            estados_cobertos = coberto 
+
+    estados_abranger -= estados_cobertos
+    estacoes_final.add(melhor_estacao)
+    print(estacoes_final)
